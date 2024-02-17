4ac2f62673f18efbe097d98108cb719b6dc479ea
TuZitt <papadracula141@gmail.com>
2024-02-17 08:33:40 +0700

    init bottom bar

--- App.tsx
+++ App.tsx
@@ -1,42 +1,50 @@
 import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import FlexBox from '@/screens/FlexBox';
 import Home from '@/screens/Home';
 import LearnFirst from '@/screens/LearnFirst';
 
 const Stack = createNativeStackNavigator();
+const BottomBar = createBottomTabNavigator<any>();
 
-export type RootStackNavigateParamList = {
-  Home: undefined;
-  Detail: undefined;
+const BottomTabBar = () => {
+  return (
+    <BottomBar.Navigator
+      screenOptions={() => ({
+        tabBarStyle: {
+          height: 60,
+          position: 'absolute',
+          bottom: 16,
+          left: 16,
+          right: 16,
+          borderRadius: 30,
+        },
+        tabBarLabelStyle: {
+          marginBottom: 5,
+        },
+        tabBarIconStyle: {
+          marginTop: 5,
+        },
+      })}
+    >
+      <BottomBar.Screen name='Home' component={Home} />
+      <BottomBar.Screen name='FlexBox' component={FlexBox} />
+      <BottomBar.Screen name='LearnFirst' component={LearnFirst} />
+    </BottomBar.Navigator>
+  );
 };
 
 function App(): React.JSX.Element {
-  // const [listAnime, setListAnime] = useState();
-  // console.log(listAnime);
-
-  // const fetchListAnime = async () => {
-  //   try {
-  //     const fetchedData = await axios.get('https://api.jikan.moe/v4/top/anime');
-  //     return setListAnime(fetchedData.data.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchListAnime();
-  // }, []);
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='bottom-bar' screenOptions={{ headerShown: false }}>
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='FlexBox' component={FlexBox} />
         <Stack.Screen name='LearnFirst' component={LearnFirst} />
+        <Stack.Screen name='bottom-bar' component={BottomTabBar} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/FlexBox/index.tsx
+++ src/screens/FlexBox/index.tsx
@@ -1,29 +1,32 @@
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Button, ScrollView, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const FlexBox = () => {
-  const navigation = useNavigation<any>();
-
   return (
-    <ScrollView>
-      <View>
-        <Text>aaaaaa</Text>
-        <Text>bbbbbb</Text>
-        <Text>cccccc</Text>
-      </View>
-      <View>
-        <Button
-          title="Go to Home"
-          onPress={() => navigation.navigate('Home')}
-        />
-        <Button
-          title="Go to Learn First"
-          onPress={() => navigation.navigate('LearnFirst')}
-        />
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.box}>aaaaaa</Text>
+      <Text style={styles.box}>bbbbbb</Text>
+      <Text style={styles.box}>cccccc</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // Mặc định thì block luôn có flex theo chiều dọc từ trên xuống
+    // flex: 1 là để chỉ định chiếm hết màn hình
+    flex: 1,
+    // flexDirection: 'row',
+    // D.m vì là mặc định chia theo trục dọc nên lộn chiều với flex-box truyền thống CSS
+    justifyContent: 'flex-end',
+  },
+  box: {
+    padding: 10,
+    backgroundColor: '#3bc496',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+});
 
 export default FlexBox;
